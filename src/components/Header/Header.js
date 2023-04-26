154cdd1d0f47d2dde9299d9e90edeba59069cb3c
riky97 <71893803+riky97@users.noreply.github.com>
2023-04-26 23:17:54 +0200

    V 1.0.3 - New implementation in header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,50 +1,110 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { useNavigate } from "react-router-dom";
 
 // antd
-import { Image, Menu } from "antd";
+import { Col, Menu, Row } from "antd";
 import { Header as HeaderComponent } from "antd/es/layout/layout";
 
 // logo
 import LogoBdiscom from "../../resources/logo/Bdiscom_srl.png";
 
+// icon
+import UserIcon from "../../resources/svg-components/UserIcon";
+import AngleSmallDown from "../../resources/svg-components/AngleSmallDown";
+
 // css
 import "./Header.css";
 
 const Header = () => {
+  const [openSubMenu, setOpenSubMenu] = useState(false);
   const navigate = useNavigate();
 
-  const items1 = ["about", "products", "contact", "login"].map((key) => ({
+  const openMegaMenuOrNavigate = (key) => {
+    switch (key) {
+      case "login":
+        return console.log("navigate");
+      case "products":
+        return setOpenSubMenu(true);
+      default:
+        break;
+    }
+  };
+
+  const itemMenu = [
+    "Who we are",
+    "What we do",
+    "Products",
+    "Contact",
+    "About",
+    "login",
+  ].map((key) => ({
     key,
-    label: key,
-    onClick: () => {
-      console.log("key", key);
-      // navigate(`/${key}`);
-    },
-    className: `menu-${key}`,
+    icon: key === "products" && <AngleSmallDown color="#2d56a0" />,
+    label: key === "login" ? <UserIcon /> : key,
+    onClick: () => openMegaMenuOrNavigate(key),
+    className: key === "login" && "menu-login--transparent",
   }));
 
   return (
-    // <section className="header">
-    <HeaderComponent>
-      <div className="logo">
+    <>
+      {openSubMenu && (
         <div
-          className="logo__image"
-          onClick={() => {
-            // navigate("/");
-          }}
+          className="header__backdrop"
+          onClick={() => setOpenSubMenu(false)}
+        ></div>
+      )}
+
+      <HeaderComponent>
+        <div className="logo">
+          <div
+            className="logo__image"
+            onClick={() => {
+              // navigate("/");
+            }}
+          />
+        </div>
+
+        <Menu
+          theme="light"
+          mode="horizontal"
+          defaultSelectedKeys={["home"]}
+          items={itemMenu}
         />
-      </div>
-      <Menu
-        theme="light"
-        mode="horizontal"
-        defaultSelectedKeys={["home"]}
-        items={items1}
-      />
-      {/* <div className="login" /> */}
-    </HeaderComponent>
-    // </section>
+
+        {/* <Menu
+          theme="light"
+          mode="horizontal"
+          style={{ justifyContent: "space-between" }}
+        >
+          <Menu.ItemGroup className="menu__left">
+            {leftMenu.map((ele) => (
+              <Menu.Item key={ele.key} onClick={ele.onClick}>
+                {ele.label}
+              </Menu.Item>
+            ))}
+          </Menu.ItemGroup>
+          <Menu.ItemGroup className="menu__right">
+            {loginMenu.map((ele) => (
+              <Menu.Item key={ele.key} onClick={ele.onClick} icon={ele.icon}>
+                {ele.label}
+              </Menu.Item>
+            ))}
+          </Menu.ItemGroup>
+        </Menu> */}
+      </HeaderComponent>
+      {openSubMenu && (
+        <Row className="mega-menu mega-menu--active">
+          <Col span={6}></Col>
+          <Col span={12}>
+            <h1>Product</h1>
+          </Col>
+          <Col span={6}>
+            <h1>X</h1>
+          </Col>
+        </Row>
+      )}
+    </>
   );
 };
 
---
+++ src/resources/svg-components/AngleSmallDown.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const UserIcon = (props) => {
+  return (
+    <svg
+      version="1.1"
+      id="Outline"
+      xmlns="http://www.w3.org/2000/svg"
+      x="0px"
+      y="0px"
+      width="25px"
+      viewBox="0 0 512 512"
+      color="#2d56a0"
+    >
+      <style type="text/css"></style>
+      <path
+        className="st0"
+        d="M399.1,175.1c-8.3-8.4-21.8-8.4-30.2-0.1c0,0-0.1,0.1-0.1,0.1l-97.7,97.7c-8.3,8.4-21.8,8.4-30.2,0.1
+	c0,0-0.1-0.1-0.1-0.1l-97.7-97.7c-8.3-8.4-21.8-8.4-30.2-0.1c0,0-0.1,0.1-0.1,0.1c-8.3,8.3-8.3,21.8,0,30.1l97.9,97.9
+	c25,25,65.5,25,90.5,0l97.9-97.9C407.4,196.9,407.4,183.5,399.1,175.1z"
+      />
+    </svg>
+  );
+};
+
+export default UserIcon;
