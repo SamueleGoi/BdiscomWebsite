388b38d251dd75a3b51ccf281ccb94b91b8fd40c
riky97 <71893803+riky97@users.noreply.github.com>
2023-07-29 11:53:17 +0200

    modifiche footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,12 +7,55 @@ import { Col, Row } from "antd";
 import LogoBdiscomWhite from "../../resources/logo/logo bianco.svg";
 
 import "./Footer.css";
+import { useNavigate } from "react-router-dom";
 
 // Ant Design ©2023 Created by Ant UED
 const Footer = () => {
+  const navigate = useNavigate();
+
+  const itemMenu = [
+    "Who we are",
+    "What we do",
+    "Products",
+    "Work with us",
+    "Contacts",
+    "Login",
+  ].map((element) => ({
+    key: element,
+    label: element,
+    onClick: (element) => clickToNavigate(element),
+  }));
+
+  const clickToNavigate = (element) => {
+    switch (element.key) {
+      case "Who we are":
+        return navigate("/about");
+      case "Login":
+        return navigate("/account/login");
+      default:
+        break;
+    }
+  };
   return (
     <FooterComponent className="footer__section">
-      <Row className="footer__section--border-top-bottom footer__section--padding">
+      <Row style={{ width: "100%" }}>
+        <Col md={8} lg={8} xl={8}>
+          <Row>
+            {itemMenu.map((ele) => {
+              return (
+                <Col
+                  key={ele.key}
+                  className="footer_section__link d-flex justify-content-center"
+                >
+                  <a onClick={ele.onClick}>{ele.label}</a>
+                </Col>
+              );
+            })}
+          </Row>
+        </Col>
+        <Col md={4} lg={4} xl={4}></Col>
+      </Row>
+      {/* <Row className="footer__section--border-top-bottom footer__section--padding">
         <Col sm={24} className="d-flex justify-content-center">
           <img
             className="footer__selection--logo-white"
@@ -33,7 +76,7 @@ const Footer = () => {
             </Col>
           </Row>
         </Col>
-      </Row>
+      </Row> */}
       {/* <Row>
         <Col sm={8} className="d-flex justify-content-center">
           <img
